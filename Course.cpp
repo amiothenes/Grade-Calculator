c44f950c8a6b57ff060db7f1c7e8b974911e998e
Amiothenes <60746276+OctavianJulius@users.noreply.github.com>
2025-05-02 19:52:14 -0400

    developed proper Course class with vars and funcs

--- Course.cpp
+++ Course.cpp
@@ -1,17 +1,36 @@
 #include "Course.h"
 
-Course::Course(std::string courseCode) {
+Course::Course(std::string courseCode, std::vector<Assessment> assessments, bool isA5050Course) {
     this->courseCode = courseCode;
+    this->assessments = assessments;
+    this->isA5050Course = isA5050Course;
 }
 
 std::string Course::getCourseCode() {
     return courseCode;
 }
 
+std::vector<Assessment> Course::getAllAssessments() {
+    return assessments;
+}
+
+bool Course::getIsA5050Course() {
+    return isA5050Course;
+}
+
 void Course::setCourseCode(std::string newCourseCode) {
     courseCode = newCourseCode;
 }
 
+void Course::setAssessments(std::vector<Assessment> newAssessments) {
+    assessments = newAssessments;
+}
+
+void Course::setIsA5050Course(bool newIsA5050Course) {
+    isA5050Course = newIsA5050Course;
+}
+
+// Assessment Management
 void Course::addAssessment(const Assessment& assessment) {
     assessments.push_back(assessment);
 }
@@ -30,16 +49,34 @@ int Course::getAssessmentCount() const {
     return assessments.size();
 }
 
-double Course::calculateOverallGrade() const {
+double Course::getTotalWeight() const {
     double totalWeight = 0.0;
-    double weightedSum = 0.0;
 
     for (const Assessment& assessment : assessments) {
         if (assessment.getIsComplete()) {
             totalWeight += assessment.getWeight();
+        }
+    }
+
+    return totalWeight;
+}
+
+bool Course::isTotalWeightValid() const {
+    double totalWeight = getTotalWeight();
+
+    return totalWeight > 0.0 && totalWeight <= 100.0 ? true : false;
+}
+
+double Course::calculateOverallGrade() const {
+    double weightedSum = 0.0;
+    bool totalWeigthValidity = isTotalWeightValid();
+
+    for (const Assessment& assessment : assessments) {
+        if (assessment.getIsComplete()) {
             weightedSum += assessment.getGrade() * assessment.getWeight();
         }
     }
 
-    return totalWeight > 0 && totalWeight <=100 ? weightedSum / totalWeight : 0.0;
+    return totalWeigthValidity ? weightedSum : 0.0;
 }
+
--- Course.h
+++ Course.h
@@ -16,25 +16,26 @@ private:
 public:
 
     //constructor
-    Course(std::string courseCode, std::vector<Assessment> assessments, bool isA5050Course) {
-        this->courseCode = courseCode;
-        this->assessments = assessments;
-        this->isA5050Course = isA5050Course;
-    }
+    Course(std::string courseCode, std::vector<Assessment> assessments, bool isA5050Course);
 
     //getter
     std::string getCourseCode();
+    std::vector<Assessment> getAllAssessments();
+    bool getIsA5050Course();
 
     //setter
     void setCourseCode(std::string newCourseCode);
+    void setAssessments(std::vector<Assessment> newAssessments);
+    void setIsA5050Course(bool newIsA5050Course);
 
     //assessment management
     void addAssessment(const Assessment& assessment);
     void removeAssessment(int index);
     Assessment& getAssessment(int index);
-    std::vector<Assessment&> getAllAssessment();
-    int getTotalWeight() const;
+
+    double getTotalWeight() const;
     int getAssessmentCount() const;
+    bool isTotalWeightValid() const;
     double calculateOverallGrade() const;
 
 };
